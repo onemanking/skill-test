41eb43b8b4e93489c855d6e57f551028c4f59db6
whai kung <nattapong.rat@mtel.co.th>
2025-05-26 12:50:50 +0700

    clean code

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,15 +1,6 @@
 import express, { Request, Response } from 'express';
 import cors from "cors";
 import { PrismaClient } from '@prisma/client';
-// import { movieRepository } from "./db/data-source";
-import { Languages } from './prisma/sql/index';
-
-const countries = [
-  'Afghanistan', 'Albania', 'Algeria', 'Argentina', 'Australia',
-  'Brazil', 'Canada', 'Denmark', 'Egypt', 'France', 'Germany',
-  'India', 'Indonesia', 'Japan', 'Kenya', 'Mexico', 'Nepal', 'Norway',
-  'Pakistan', 'Qatar', 'Russia', 'Singapore', 'Spain', 'Thailand', 'USA'
-];
 
 const app = express();
 const prisma = new PrismaClient();
@@ -17,7 +8,7 @@ const prisma = new PrismaClient();
 app.use(cors());
 app.use(express.json());
 
-app.get("/", (req, res) => {
+app.get("/", (req: Request, res: Response) => {
   res.send("Hello World!");
 });
 
