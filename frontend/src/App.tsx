0a143e6a5e7b252f95b062bb80996bebcc9a843f
Rachen <rachen@miimo.ai>
2025-06-10 12:52:43 +0700

    feat: implement search functionality and display country data in the app

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,43 @@
 import './App.css'
+import ResultTable from './components/ResultTable';
+import Searchbox from './components/Searchbox'
+import { useState } from 'react'
+
+const baseUrl = import.meta.env.VITE_API_URL || "http://localhost:3000";
 
 function App() {
+  const [search, setSearch] = useState("");
+  const [countries, setCountries] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  const handleSearch = async () => {
+    setLoading(true);
+    try {
+      const apiURL = `${baseUrl}/countries`;
+      const url = search ? `${apiURL}?search=${encodeURIComponent(search)}` : apiURL;
+      const res = await fetch(url);
+      const data = await res.json();
+      setCountries(data);
+    } catch (err) {
+      console.error("API error:", err);
+      setCountries([]);
+    }
+    setLoading(false);
+  };
 
   return (
     <>
       <div className="w-full">
         <div>
-          <p>Skill test</p>          
+          <p>Countries of the World</p>
+          <Searchbox
+            search={search}
+            setSearch={setSearch}
+            onSearch={handleSearch}
+            loading={loading}
+          />
+          <ResultTable
+            countries={countries} />
         </div>
       </div>
     </>
